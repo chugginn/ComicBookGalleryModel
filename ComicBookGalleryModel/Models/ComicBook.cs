92422d6c2034b83e4e68b9a15ee1440d298d806a
Mark Ramos <markramos83@gmail.com>
2018-10-19 09:11:41 -0600

    Explicity define Series ID property on the Comic Book entity.

--- ComicBookGalleryModel/Models/ComicBook.cs
+++ ComicBookGalleryModel/Models/ComicBook.cs
@@ -9,12 +9,19 @@ namespace ComicBookGalleryModel.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        // many-to-one relationship created by the following 'navigation' property:
-        public Series Series { get; set; }
         public int IssueNumber { get; set; }
+        // explicitly naming this property 'Series' with 'Id' is a convention that EF uses to
+        // reference this ID field with the Series navigation property below. Doing
+        // this ensures that Series is non-nullable, since int is non-nullable.
+        // Also, this allows us to associate a Comicbook with a Series just by providing
+        // the Series ID.
+        public int SeriesId { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
         public decimal? AverageRating { get; set; }
+
+        // many-to-one relationship created by the following 'navigation' property:
+        public Series Series { get; set; }
 
         public string DisplayText
         {
