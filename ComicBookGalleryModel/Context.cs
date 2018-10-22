f83d3670f5abf3f6b743f6c0f26ecfb20b93d8cf
Mark Ramos <markramos83@gmail.com>
2018-10-22 11:04:38 -0600

    Created a custom DatabaseInitializer to seed our database.

--- ComicBookGalleryModel/Context.cs
+++ ComicBookGalleryModel/Context.cs
@@ -19,7 +19,8 @@ namespace ComicBookGalleryModel
             // the code below.
             //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
             //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            Database.SetInitializer(new DatabaseInitializer());
         }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
