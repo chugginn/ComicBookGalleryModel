9264833b7553cfe2863d5ccae8c2839c9d73ac05
Mark Ramos <markramos83@gmail.com>
2018-10-23 12:18:32 -0600

    Retrieve single records using LINQ

--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -20,33 +20,47 @@ namespace ComicBookGalleryModel
                 // this line will log all our SQL queries to the output window
                 context.Database.Log = (message) => Debug.WriteLine(message);
 
+                var comicBookId = 1;
+
+                //var comicBook1 = context.ComicBooks.Find(comicBookId);
+                //var comicBook2 = context.ComicBooks.Find(comicBookId);
+
+                var comicBook1 = context.ComicBooks
+                    .SingleOrDefault(cb => cb.Id == comicBookId);
+
+                Debug.WriteLine("Changing the description property value.");
+                comicBook1.Description = "New value!";
+
+                var comicBook2 = context.ComicBooks
+                    .SingleOrDefault(cb => cb.Id == comicBookId);
+
                 // the DbSet ToList method retrieves a list of entity instances.
                 // with a relationship, like Comic Books to Series, we need
                 // to be explicit and include the Series data. If we don't,
                 // Comicbook.Series would be NULL. We can use a Linq Expression
                 // to express the Series Entity
-                var comicBooks = context.ComicBooks
-                    .Include(cb => cb.Artists.Select(a => a.Artist))
-                    .Include(cb => cb.Artists.Select(a => a.Role))
-                    .Include(cb => cb.Series)
-                    .ToList();
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Artists.Select(a => a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .Include(cb => cb.Series)
+                //    .ToList();
 
-                // use foreach to loop through all instances of comic books
-                // and write the series title property to the console
-                foreach (var comicBook in comicBooks)
-                {
+                //// use foreach to loop through all instances of comic books
+                //// and write the series title property to the console
+                //foreach (var comicBook in comicBooks)
+                //{
 
-                    // include a list of each comic book's artists. First, create a variable
-                    // that uses LINQ to transform a collection of Artist entity objects to a
-                    // list of Artist names:
-                    var artistRoleNames = comicBook.Artists
-                        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
-                    // then use Join() to convert the collection of strings to a comma-delimited list
-                    var artistRoleDisplayText = string.Join(", ", artistRoleNames);
+                //    // include a list of each comic book's artists. First, create a variable
+                //    // that uses LINQ to transform a collection of Artist entity objects to a
+                //    // list of Artist names:
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                //    // then use Join() to convert the collection of strings to a comma-delimited list
+                //    var artistRoleDisplayText = string.Join(", ", artistRoleNames);
 
-                    Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistRoleDisplayText);
-                }
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRoleDisplayText);
+                //}
                 Console.ReadLine();
             }
 
